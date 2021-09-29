f20c54cec1b93d63f832379f60482e7beed958d6
Grayson Hardin <grayhardin@gmail.com>
2021-09-29 18:49:20 -0500

    Added a test to further validate that the min value is within the range. Test passed.

--- Test/test_validator.py
+++ Test/test_validator.py
@@ -4,14 +4,20 @@ from Production.validator import validate_input
 
 
 class TestValidator(unittest.TestCase):
-    def test_validator_value_under_range(self):
+    def test_validator_value_is_below_min_range(self):
         actual = validate_input(0)
         expected = "Error, value must be between 1 and 5000"
 
         self.assertEqual(actual, expected)
 
-    def test_validator_value_above_range(self):
+    def test_validator_value_is_above_max_range(self):
         actual = validate_input(50001)
         expected = "Error, value must be between 1 and 5000"
+
+        self.assertEqual(actual, expected)
+
+    def test_validator_within_min_range(self):
+        actual = validate_input(1)
+        expected = None
 
         self.assertEqual(actual, expected)
