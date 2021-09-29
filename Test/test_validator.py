6f39fcf78d9ab419e1f72af8e1ad5bacae81b88c
Grayson Hardin <grayhardin@gmail.com>
2021-09-29 16:48:36 -0500

    Added an additional test to handle when the user_input is above 5000 (that is the max). Test failed.

--- Test/test_validator.py
+++ Test/test_validator.py
@@ -5,7 +5,13 @@ from Production.validator import validate_input
 
 class TestValidator(unittest.TestCase):
     def test_validator_value_under_range(self):
-        actual = validate_input(-1)
+        actual = validate_input(0)
         expected = "Error, value must be above 1"
+
+        self.assertEqual(actual, expected)
+
+    def test_validator_value_above_range(self):
+        actual = validate_input(50001)
+        expected = "Error, value must be less than 5000"
 
         self.assertEqual(actual, expected)
