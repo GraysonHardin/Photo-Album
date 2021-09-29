8a6b431e5c15436d0cf723d81659e0d5e0446b7c
Grayson Hardin <grayhardin@gmail.com>
2021-09-29 16:51:00 -0500

    Updated the validator.py logic so that it handles both conditions on one line. Return values were updated and both tests from test_validator.py pass.

--- Production/validator.py
+++ Production/validator.py
@@ -1,3 +1,3 @@
 def validate_input(user_input):
-    if user_input < 0:
-        return "Error, value must be above 1"
+    if user_input < 1 or user_input > 5000:
+        return "Error, value must be between 1 and 5000"
--- Test/test_validator.py
+++ Test/test_validator.py
@@ -6,12 +6,12 @@ from Production.validator import validate_input
 class TestValidator(unittest.TestCase):
     def test_validator_value_under_range(self):
         actual = validate_input(0)
-        expected = "Error, value must be above 1"
+        expected = "Error, value must be between 1 and 5000"
 
         self.assertEqual(actual, expected)
 
     def test_validator_value_above_range(self):
         actual = validate_input(50001)
-        expected = "Error, value must be less than 5000"
+        expected = "Error, value must be between 1 and 5000"
 
         self.assertEqual(actual, expected)
