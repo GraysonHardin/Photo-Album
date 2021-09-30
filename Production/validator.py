9671c37809c5959579832e48e7e74c64f1b5a3cd
Grayson Hardin <grayhardin@gmail.com>
2021-09-29 19:03:08 -0500

    Implemented a try/catch in the validator.py. Test passes.

--- Production/validator.py
+++ Production/validator.py
@@ -1,3 +1,7 @@
 def validate_input(user_input):
-    if user_input < 1 or user_input > 5000:
-        return "Error, value must be between 1 and 5000"
+    try:
+        if int(user_input) < 1 or int(user_input) > 5000:
+            return "Error, value must be between 1 and 5000"
+
+    except ValueError:
+        return 'Invalid input! Try again'
