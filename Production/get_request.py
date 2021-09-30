0821a98311d89889a860550b2ec5697061c62d2d
Grayson Hardin <grayhardin@gmail.com>
2021-09-30 15:50:03 -0500

    Added try/catch to get_request. Test passes.

--- Production/get_request.py
+++ Production/get_request.py
@@ -2,6 +2,10 @@ import requests
 
 
 def get_album_id_request(album_id):
-    r = requests.get(f'https://jsonplaceholder.typicode.com/photos/{album_id}')
-    return r.json()
+    try:
+        r = requests.get(f'https://jsonplaceholder.typicode.com/photos/{album_id}')
+        return r.json()
+
+    except Exception as err:
+        raise Exception('Error occurred', err)
 # add a try/catch later
