0b25aede8df6f41c2eba9dc828593d880e11cf1c
Grayson Hardin <grayhardin@gmail.com>
2021-10-02 10:39:03 -0500

    Added a second if statement to ensure that the values are within the range. Tests passed.

--- Production/validator.py
+++ Production/validator.py
@@ -3,5 +3,8 @@ def validate_input(user_input):
         if int(user_input) < 1 or int(user_input) > 5000:
             return False
 
+        elif int(user_input) >= 1 or int(user_input) <= 5000:
+            return True
+
     except ValueError:
         return 'Invalid input! Try again'
