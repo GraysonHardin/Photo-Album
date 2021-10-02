3144fd8cec8814efcd1d2a17b547fdef07a11306
Grayson Hardin <grayhardin@gmail.com>
2021-10-02 13:15:08 -0500

    Added If statements to main() to handle program termination and validation.

--- Production/main.py
+++ Production/main.py
@@ -1,9 +1,22 @@
 from Production.get_request import get_album_id_request
+from Production.validator import validate_input
 
 
 def main():
-    user_input = input("Enter an ID value: ")
-    print(get_album_id_request(user_input))
+    while True:
+        user_input = input("Enter an ID value: ").lower()
+        if str(user_input) == 'quit':
+            print('Program ended')
+            return False
+
+        elif validate_input(user_input) == False:
+            print('Error, value must be between 1 and 5000')
+
+        elif validate_input(user_input) == True:
+            print(get_album_id_request(user_input))
+
+        else:
+            print(validate_input(user_input))
 
 
 main()
